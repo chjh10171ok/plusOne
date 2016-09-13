735bcd9b66dc804e8161b0a9f21769ec58312573
Howard You <chjh_10171ok@yahoo.com.tw>
2016-09-13 19:54:51 +0800

    將header中的nav分出來，單獨成立php檔案

--- inc/config.php
+++ inc/config.php
@@ -4,9 +4,21 @@
 //----------------------------------------------------------
 require_once(dirname(__FILE__) . "/config_db.php");
 
+//----------------------------------------------------------
+// 網站實體路徑常數
+//----------------------------------------------------------
 define("BASE_ROOT", "/wwwroot/git/plusOne/"); // 系統實體路徑位置
 
 define("DOC_ROOT", "/git/plusOne"); // 系統網站目錄
 
 define("WEB_ROOT", "http://ds.nb.howard" . DOC_ROOT); // 線上網站 Domain
+
+//----------------------------------------------------------
+// 網站相關名稱常數
+//----------------------------------------------------------
+define("WEB_PAGE_CHARSET", "utf-8"); // 網站網頁編碼
+define("WEB_DESCRIPTION", "Free Web tutorials"); // 網站說明
+define("WEB_KEYWORDS", "HTML, CSS, XML, JavaScript"); // 網站關鍵字
+define("WEB_AUTHOR_NAME", "Howard You"); // 網站作者
+define("WEB_PAGE_TITLE", "Plus One ! 加我!"); // 網站作者
 ?>
--- inc/header.php
+++ inc/header.php
@@ -4,14 +4,15 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+
+<head>
+    <meta charset="<?php echo WEB_PAGE_CHARSET; ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Free Web tutorials">
-    <meta name="keywords" content="HTML, CSS, XML, JavaScript">
-    <meta name="author" content="Howard You">
-    <title>Page Title</title>
+    <meta name="description" content="<?php echo WEB_DESCRIPTION; ?>">
+    <meta name="keywords" content="<?php echo WEB_KEYWORDS; ?>">
+    <meta name="author" content="<?php echo WEB_AUTHOR_NAME; ?>">
+    <title><?php echo WEB_PAGE_TITLE; ?></title>
     <link href="<?php echo WEB_ROOT; ?>/asset/default/c/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo WEB_ROOT; ?>/asset/default/c/bootstrap-theme.min.css" rel="stylesheet">
     <!-- jQuery (Bootstrap 所有外掛均需要使用) -->
@@ -45,31 +46,7 @@
 </head>
 
 <body role="document">
-
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand" href="#">Bootstrap theme</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li><li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li><li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li><li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php require_once(BASE_ROOT . "/inc/header-nav.php"); ?>
 
 <!-- start container -->
 <div class="container">
