a0ed6d8387a3e4597dd0f34745ac071c8c951b35
Howard You <chjh_10171ok@yahoo.com.tw>
2016-09-22 18:04:32 +0800

    修改header-nav.php文字 新增根據PAGE_TAG常數，顯示active

--- inc/header-nav.php
+++ inc/header-nav.php
@@ -1,14 +1,23 @@
+<?php
+if (defined(PAGE_TAG)) { define('PAGE_TAG', 'index'); }
+?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand" href="#">Plus One！</a>
+            <a class="navbar-brand" href="<?php echo WEB_ROOT; ?>">Plus One！</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="<?php echo WEB_ROOT; ?>">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li <?php echo ('index' === PAGE_TAG) ? 'class="active"' : ''; ?>>
+                    <a href="<?php echo WEB_ROOT; ?>">Home</a>
+                </li>
+                <li <?php echo ('project' === PAGE_TAG) ? 'class="active"' : ''; ?>>
+                    <a href="<?php echo WEB_ROOT . '/' . PAGE_TAG . '.php'; ?>">Project</a>
+                </li>
+                <li <?php echo ('index' === PAGE_TAG) ? 'class="active"' : ''; ?>>
+                    <a href="#contact">Contact</a>
+                </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
